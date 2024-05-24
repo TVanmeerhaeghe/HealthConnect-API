9b90810981c7c41153ff33f65b9c1c0cd46017a4
Teo <vanmeerhaegheteo@gmail.com>
2024-05-24 11:50:34 +0200

    Fix : Fixed config file for eslint (rm unusued variable)

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// eslint.config.mjs
+
+// import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+
+// const compat = new FlatCompat({
+//   baseDirectory: import.meta.url,
+// });
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+    },
+  },
+];
