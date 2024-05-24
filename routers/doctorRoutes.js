620dc3fc8189374dfd1fede77d019810767f1828
Teo <vanmeerhaegheteo@gmail.com>
2024-05-24 16:16:28 +0200

    Feat : Added routes for Get GetId et search query for doctor

--- routers/doctorRoutes.js
+++ routers/doctorRoutes.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getAllDoctors,
+  getDoctorById,
+  searchDoctors,
+} = require('../models/doctorModel');
 
 router.use(express.json());
 
-router.get('/', (req, res) => {
-  res.send('List of all doctors');
+router.get('/', async (req, res) => {
+  try {
+    const doctors = await getAllDoctors();
+    res.json(doctors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const doctorId = req.params.id;
-  res.send(`Doctor with ID ${doctorId}`);
+  try {
+    const doctor = await getDoctorById(doctorId);
+    res.json(doctor);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
+
+router.get('/search/:query', async (req, res) => {
+  const searchQuery = req.params.query;
+  try {
+    const doctors = await searchDoctors(searchQuery);
+    res.json(doctors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
